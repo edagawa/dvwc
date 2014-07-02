a2ca832531ad4a44fdfa798e4f5634c7a0543058
kiku1monzi3622 <team.ganeme@gmail.com>
2014-07-03 03:04:53 +0900

    グラフを表示

--- worldcup.js
+++ worldcup.js
@@ -1,35 +1,42 @@
-var winner = {
-  viewWidth:  900,
-  viewHeight: 650,
-  svg:        null,
-  homeWin:    0,
-  awayWin:    0,
-  draw:       0,
+var worldCup = {
+  viewWidth:   400,
+  viewHeight:  400,
+  outerRadius: 200,
+  innerRadius: 0,
+  svg:         null,
+  arcs:        null,
+  arc:         null,
+  pie:         d3.layout.pie(),
+  color:       d3.scale.category10(),
+  homeWin:     0,
+  awayWin:     0,
+  draw:        0,
+  gameData:    [],
 
   init: function() {
     this.loadJson();
-    this.createViewArea();
+    this.createSvg();
+    this.createArc();
   },
 
-  //svgを描画
-  createViewArea: function() {
-    svg = d3.select('#view').append('svg')
+  //svgを作成
+  createSvg: function() {
+    worldCup.svg = d3.select('#view').append('svg')
           .attr('width', this.viewWidth)
           .attr('height', this.viewHeight);
-    this.renderText(this.homeWin);
   },
 
-  renderText: function(homeWin) {
-
-    d3.select("svg").append("p")
-      .style("font", "18px sans-serif")
-      .text(homeWin);
+  //arcを作成
+  createArc: function() {
+    worldCup.arc = d3.svg.arc()
+         .innerRadius(worldCup.innerRadius)
+         .outerRadius(worldCup.outerRadius);
   },
 
   //ワールドカップのJSONファイルをロード
   loadJson: function() {
     d3.json("http://worldcup.sfg.io/matches.json", function(json) {
-      winner.getWinner(json);
+      worldCup.getWinner(json);
     });
   },
 
@@ -50,20 +57,69 @@ var winner = {
       if(gameStatus === 'completed') {
         //ホームチームが勝った場合ホームチームに１プラス
         if(gameWinner === homeTeam) {
-          winner.homeWin++;
+          worldCup.homeWin++;
         //アウェイチームが勝った場合アウェイチームに１プラス
         } else if (gameWinner === awayTeam) {
-          winner.awayWin++;
+          worldCup.awayWin++;
         //どちらも勝っていない場合drawに１プラス
         } else {
-          winner.draw++;
+          worldCup.draw++;
         }
       }
     }
+    worldCup.setGameData();
+  },
 
+  //試合結果のデータを設定
+  setGameData: function() {
+    worldCup.gameData.push(worldCup.homeWin, worldCup.awayWin, worldCup.draw);
+    worldCup.setArcs();
+  },
+
+  //arcをセット
+  setArcs: function() {
+    worldCup.arcs = worldCup.svg.selectAll("g.arc")
+           .data(worldCup.pie(worldCup.gameData))
+           .enter()
+           .append("g")
+           .attr("class", "arc")
+           .attr("transform", "translate(" + worldCup.outerRadius + "," + worldCup.outerRadius + ")");
+    worldCup.drawArcs();
+    worldCup.viewLabels();
+  },
+
+  //arcを描画
+  drawArcs: function() {
+    worldCup.arcs.append("path")
+        .attr("fill", function(d, i) {
+          return worldCup.color(i);
+        })
+        .attr("d", worldCup.arc)
+        .on('mouseover', function(d){
+          worldCup.zoomIn();
+        });
+  },
+
+  //arcをセット
+  viewLabels: function() {
+    worldCup.arcs.append("text")
+        .attr("transform", function(d) {
+          return "translate(" + worldCup.arc.centroid(d) + ")";
+        })
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+          return d.value;
+        });
+  },
+
+  //arcをセット
+  zoomIn: function(p) {
+    if (p.depth > 1) p = p.parent;
+    if (!p.children) return;
+    zoom(p, p);
   }
 };
 
 window.onload = function() {
-  winner.init();
+  worldCup.init();
 }
